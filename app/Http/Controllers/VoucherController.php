c55c93ba8c192080a4406141b62b5b732d5324b6
Sajida baloch <sajidairfanbaloch@gmail.com>
2026-07-09 16:15:59 +0500

    update code

---
+++ app/Http/Controllers/VoucherController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Voucher;
+use Illuminate\Http\Request;
+
+class VoucherController extends Controller
+{
+    public function store(Request $request){
+            Voucher::create($request->all());
+            return "Voucher Save Successfully";
+    }
+}
